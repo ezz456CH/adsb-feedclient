3578ff44f977ceabb44e27cb3653835547f5e793
ezz456CH <147528579+ezz456CH@users.noreply.github.com>
2024-02-01 15:14:28 +0700

    update

--- configure.sh
+++ configure.sh
@@ -138,6 +138,6 @@ PRIVACY=""
 INPUT_TYPE="$INPUT_TYPE"
 
 MLATSERVER=""
-TARGET="--net-connector adsb.ezz456ch.xyz,30004,beast_reduce_out"
+TARGET="--net-connector feed.ezz456ch.xyz,30004,beast_reduce_out"
 NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30154 --net-bo-port 0 --net-ri-port 0 --write-json-every 1"
 JSON_OPTIONS="--max-range 450 --json-location-accuracy 2 --range-outline-hours 24"
