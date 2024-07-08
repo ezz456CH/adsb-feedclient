ad681b6be19199834d5ccf799178882abb262dd1
ezz456CH <ezz456chgmok@gmail.com>
2024-07-09 03:16:52 +0700

    fix typo

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ mkdir -p $IPATH
 
 if [ "$(id -u)" != "0" ]; then
     echo -e "\033[33m"
-    echo "This script must be ran using sudo or as root."
+    echo "This script must be run using sudo or as root."
     echo -e "\033[37m"
     exit 1
 fi
